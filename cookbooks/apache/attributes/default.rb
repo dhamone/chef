71cf994994c95b2fdf9d8d478f245df684c1aa3d
dhamone <dhamlett@hotmail.com>
2015-12-10 15:46:59 +0000

    first commit

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,12 @@
+default["apache"]["sites"]["dhamlett4"] = {"site_title" => "Dhamlett4 website coming soon","port" => 80, "domain" => "dhamlett4.mylabserver.com"}
+default["apache"]["sites"]["dhamlett5"] = {"site_title" => "Dhamlett5 website coming soon","port" => 80, "domain" => "dhamlett5.mylabserver.com"}
+default["apache"]["sites"]["dhamlett3"] = {"site_title" => "donnie3 website","port" => 80, "domain" => "dhamlett3.mylabserver.com"}
+
+case node ["platform"] 
+when "centos"
+	default["apache"]["package"] = "httpd"
+when "ubuntu"
+	default["apache"]["package"] = "apache2"
+end
+
+
